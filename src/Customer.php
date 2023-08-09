f4962093dae923cc121db804d1bd5b2f60f17afc
ceikdo <jabalabdul71@gmail.com>
2023-08-09 21:33:42 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BelajarPhpComposerHello;
+
+class Customer{
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string{
+        return "Hello $name, My Name is $this->name" . PHP_EOL;
+    }
+}
